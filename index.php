473a78382ad89f7c244454efadc22c027324ec63
salzanopm <salzanopm@gmail.com>
2022-01-27 17:26:10 +0100

    finish

--- index.php
+++ index.php
@@ -8,46 +8,48 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+    class Movie {
 
-class Movie {
+        public $title;
 
-    public $title;
+        public $director;
 
-    public $year;
+        public $year;
 
-    public $rate = 0;
+        public $rate;
 
-    public function __construct($_title, $_relase_date, $_rate) {
-        $this->title = $_title;
-        $this->year = $_relase_date;
-        $this->oscar = $_rate;
+        public function __construct($_title, $_director) {
+            $this->title = $_title;
+            $this->director = $_director;
+        }
+
+        // ritorna presentazione completa del Film
+        public function getFullMovieName() {
+            return $this->title . ' diretto da ' . $this->director . ' uscito nel ' . $this->year . ' con un punteggio di ' . $this->rate;
+        }
     }
-
-    // ritorna presentazione completa del Film
-    public function getFullMovieName() {
-        return $this->title . 'uscito nel' . $this->year . 'con un punteggio di' . $this->rate;
-    }
-
-}
 ?>
 
 <!-- elenco film -->
 <?php 
-    $the_karate_kid = new Movie('The Karate Kid');
-    $the_karate_kid-> year = '1984';
-    $the_karate_kid-> rate = '7.2';
+    $the_karate_kid = new Movie('The Karate Kid', 'Christopher Cain');
+    $the_karate_kid->year = '1984';
+    $the_karate_kid->rate = '7.2';
 
-    $the_karate_kid_part_II = new Movie('The Karate Kid part II');
-    $the_karate_kid_part_II-> year = '1986';
-    $the_karate_kid_part_II-> rate = '8.2';
+    $the_karate_kid_part_II = new Movie('The Karate Kid part II', 'Christopher Cain');
+    $the_karate_kid_part_II->year = '1986';
+    $the_karate_kid_part_II->rate = '8.2';
     
-    $the_karate_kid_part_III = new Movie('The Karate Kid part III');
-    $the_karate_kid_part_III-> year = '1989';
-    $the_karate_kid_part_III-> rate = '9.2';
+    $the_karate_kid_part_III = new Movie('The Karate Kid part III', 'Christopher Cain');
+    $the_karate_kid_part_III->year = '1989';
+    $the_karate_kid_part_III->rate = '9.2';
 
-    $the_karate_kid_part_IV = new Movie('The Karate Kid part IV');
-    $the_karate_kid_part_IV-> year = 'Agosto 1994';
-    $the_karate_kid_part_IV-> rate = '9.2';
+    $the_karate_kid_part_IV = new Movie('The Karate Kid part IV', 'Christopher Cain');
+    $the_karate_kid_part_IV->year = 'Agosto 1994';
+    $the_karate_kid_part_IV->rate = '9.2';
 ?>
 
 <!DOCTYPE html>
